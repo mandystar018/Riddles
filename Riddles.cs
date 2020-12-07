dafc113d83a0d0e61e77f7a0a778bafabe81cfe2
Pugsly99 <rylandadams@yahoo.com>
2020-12-07 14:35:02 -0800

    it will choose riddle at random Co-Authored-by:Amanda Otero <mandystar018@gmail.com>

--- Riddles.cs
+++ Riddles.cs
@@ -1,35 +1,81 @@
 using System;
 using System.Collections.Generic;
 
-class Riddle
+public class Riddle
 {
-  static void Main()
-  {
-    Console.WriteLine("The more I take the more I leave behind, what am I?");
-    string answer = Console.ReadLine();
-    bool word = answer == "Footsteps";
-    bool word2 = answer == "footsteps";
+  public string Question;
+  public string Answer;
 
-    if (word || word2)
+  public Riddle(string question, string answer)
+  {
+    Question = question;
+    Answer = answer;
+  }
+}
+
+public class Program
+{
+  public static void Main()
+  {
+    Random rnd = new Random();
+    
+    Riddle riddle1 = new Riddle("The more I take the more I leave behind, what am I?", "Footsteps");
+    Riddle riddle2 = new Riddle("What has to be broken before you can use it?", "Egg");
+    Riddle riddle3 = new Riddle("What month of the year has 28 days?", "All");
+    Riddle riddle4 = new Riddle("What gets wet while drying?", "Towel");
+
+    List<Riddle> Riddles = new List<Riddle>(){riddle1, riddle2, riddle3, riddle4};
+    
+    int num = rnd.Next(0,4);
+
+    string thisRiddde = Riddles[num].Question;
+    string thisRiddleAnswer = Riddles[num].Answer;
+    Console.WriteLine(thisRiddde);
+
+    string answer = Console.ReadLine();
+    bool word = answer == thisRiddleAnswer;
+
+
+    if (word)
     {
       Console.WriteLine("Correct, how are you so smart! Im defeated, onto the next question.");
     }else 
     {
       Console.WriteLine("Wrong! I ate you");
-    }
-
-    Console.WriteLine("Alright, how about another one?");
-    Console.WriteLine("What has to be broken before you can use it? Im defeated");
-    string newanswer = Console.ReadLine();
-    bool newword = newanswer == "Egg";
-    bool newword2 = newanswer == "egg";
-
-    if (newword || newword2)
-    {
-      Console.WriteLine("Correct, how are you so smart! Im defeated, onto the next question.");
-    }else 
-    {
-      Console.WriteLine("Wrong! I ate you,");
-    }
+    }  
   }
 }
+
+
+// class Riddle
+// {
+//   static void Main()
+//   {
+//     Console.WriteLine("The more I take the more I leave behind, what am I?");
+//     string answer = Console.ReadLine();
+//     bool word = answer == "Footsteps";
+//     bool word2 = answer == "footsteps";
+
+//     if (word || word2)
+//     {
+//       Console.WriteLine("Correct, how are you so smart! Im defeated, onto the next question.");
+//     }else 
+//     {
+//       Console.WriteLine("Wrong! I ate you");
+//     }
+
+//     Console.WriteLine("Alright, how about another one?");
+//     Console.WriteLine("What has to be broken before you can use it? Im defeated");
+//     string newanswer = Console.ReadLine();
+//     bool newword = newanswer == "Egg";
+//     bool newword2 = newanswer == "egg";
+
+//     if (newword || newword2)
+//     {
+//       Console.WriteLine("Correct, how are you so smart! Im defeated, onto the next question.");
+//     }else 
+//     {
+//       Console.WriteLine("Wrong! I ate you,");
+//     }
+//   }
+// }
